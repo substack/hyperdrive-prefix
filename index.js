4a70d17a34174c25941f61da5a7d8b500fb5b563
James Halliday <mail@substack.net>
2016-05-12 23:04:35 +0100

    deferred with setArchive()

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 var inherits = require('inherits')
 var EventEmitter = require('events').EventEmitter
 var through = require('through2')
-var readonly = require('read-only-stream')
 var quotemeta = require('quotemeta')
+var duplexify = require('duplexify')
 
 inherits(Wrap, EventEmitter)
 module.exports = Wrap
 
 function Wrap (prefix, archive) {
-  if (!(this instanceof Wrap)) return new Wrap(prefix, archive)
-  EventEmitter.call(this)
+  var self = this
+  if (!(self instanceof Wrap)) return new Wrap(prefix, archive)
+  EventEmitter.call(self)
+  self._prefix = prefix.replace(/\/+$/, '')
+  self._re = RegExp('^' + quotemeta(self._prefix) + '/')
+
+  if (archive) setArchive(archive)
+  else self.once('_archive', setArchive)
+
+  function setArchive (archive) {
+    self._archive = archive
+    self.key = archive.key + '/' + self._prefix
+  }
+}
+
+Wrap.prototype.setArchive = function (archive) {
   this._archive = archive
-  this._prefix = prefix.replace(/\/+$/, '')
-  this.key = archive.key + '/' + this._prefix
-  this._re = RegExp('^' + quotemeta(this._prefix) + '/')
+  this.emit('_archive', archive)
+}
+
+Wrap.prototype._getArchive = function (fn) {
+  if (this._archive) fn(this._archive)
+  else this.once('_archive', fn)
 }
 
 Wrap.prototype._add = function (entry) {
@@ -35,21 +52,34 @@ Wrap.prototype._remove = function (entry) {
 }
 
 Wrap.prototype.append = function (entry, cb) {
-  this._archive.append(this._add(entry), cb)
+  var self = this
+  this._getArchive(function (archive) {
+    archive.append(self._add(entry), cb)
+  })
 }
 
 Wrap.prototype.get = function (index, cb) {
-  this._archive.get(index, cb)
+  this._getArchive(function (archive) {
+    archive.get(index, cb)
+  })
 }
 
 Wrap.prototype.download = function (index, cb) {
-  this._archive.download(index, cb)
+  this._getArchive(function (archive) {
+    archive.download(index, cb)
+  })
 }
 
 Wrap.prototype.list = function () {
   var self = this
+  var d = duplexify.obj()
   var prefix = self._prefix
-  return readonly(self._archive.list().pipe(through.obj(write)))
+  var stream = through.obj(write)
+  d.setReadable(stream)
+  self._getArchive(function (archive) {
+    archive.list().pipe(stream)
+  })
+  return d
 
   function write (entry, enc, next) {
     if (entry.name.split('/')[0] === prefix) {
@@ -60,9 +90,19 @@ Wrap.prototype.list = function () {
 }
 
 Wrap.prototype.createFileReadStream = function (entry) {
-  return this._archive.createFileReadStream(this._add(entry))
+  var self = this
+  var d = duplexify()
+  self._getArchive(function (archive) {
+    d.setReadable(archive.createFileReadStream(self._add(entry)))
+  })
+  return d
 }
 
 Wrap.prototype.createFileWriteStream = function (entry) {
-  return this._archive.createFileWriteStream(this._add(entry))
+  var self = this
+  var d = duplexify()
+  self._getArchive(function (archive) {
+    d.setWritable(archive.createFileWriteStream(self._add(entry)))
+  })
+  return d
 }
---
+++ test/defer.js
@@ -0,0 +1,57 @@
+var test = require('tape')
+var hyperdrive = require('hyperdrive')
+var memdb = require('memdb')
+var collect = require('collect-stream')
+var concat = require('concat-stream')
+var prefix = require('../')
+
+test('defer', function (t) {
+  t.plan(9)
+  var drive = hyperdrive(memdb())
+
+  var dir1 = prefix('dir1')
+  dir1.createFileWriteStream('hello.txt').end('BEEP BOOP\n')
+  dir1.createFileWriteStream('whatever.txt').end('hey\n')
+
+  var dir2 = prefix('dir2')
+  dir2.createFileWriteStream('hello.txt').end('EHLO WORLD\n')
+
+  var archive = drive.createArchive()
+  dir1.setArchive(archive)
+  dir2.setArchive(archive)
+
+  archive.finalize(function () {
+    collect(dir1.list(), function (err, files) {
+      t.error(err)
+      t.deepEqual(
+        files.map(fname).sort(),
+        ['hello.txt','whatever.txt'].sort(),
+        'dir1 files'
+      )
+    })
+    collect(dir2.list(), function (err, files) {
+      t.error(err)
+      t.deepEqual(files.map(fname), ['hello.txt'], 'dir2 files')
+    })
+    collect(archive.list(), function (err, files) {
+      t.error(err)
+      t.deepEqual(
+        files.map(fname).sort(),
+        ['dir1/hello.txt','dir1/whatever.txt','dir2/hello.txt'].sort(),
+        'all files'
+      )
+    })
+
+    dir1.createFileReadStream('hello.txt').pipe(concat(function (buf) {
+      t.equal(buf.toString(), 'BEEP BOOP\n')
+    }))
+    dir1.createFileReadStream('whatever.txt').pipe(concat(function (buf) {
+      t.equal(buf.toString(), 'hey\n')
+    }))
+    dir2.createFileReadStream('hello.txt').pipe(concat(function (buf) {
+      t.equal(buf.toString(), 'EHLO WORLD\n')
+    }))
+  })
+})
+
+function fname (entry) { return entry.name }
